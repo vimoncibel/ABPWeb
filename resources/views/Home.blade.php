94acbd298ad91b57516092db2b134d2b0843043a
Sintia DWi Cahya <sintiadc96@gmail.com>
2024-04-23 21:04:28 +0700

    progress2

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -7,9 +7,108 @@
     <title>Login</title>
         <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/SignUp.css">
+    <link rel="stylesheet" href="/css/Home.css">
 
 </head>
 <body>   
+    <nav class="navbar navbar-expand-lg navbar-light px-5">
+            <img class="navbar-brand" src="/image/brand.png" alt="Brand Image">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="Home">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Recipes">RECIPES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Calorie">CALORIE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Create Your Recipe">CREATE YOUR RECIPE</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+
+    <!--HERO SECTION-->
+    <div class="main-section justify-content-center">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-6 tagline py-5 col-sm-12">
+                    <h1><strong>Discover Your <br> Health Secret  with <br>MealMate!</strong></h1><br>
+                    <p>
+                        Calculate your calorie consumption quickly and accurately, and find recipes that suit you! Start Your Journey Towards a Better Healthy Life with MealMate Now!
+                    </p>
+                </div>
+                <div class="Pic1Home d-flex flex-column align-items-center col-md-6 py-5 col-sm-12">
+                    <img src="/image/Pic1Home.png" class="img-fluid"/>
+                    <button id="Explore Menu"  class="register btn btn-primary mt-3">Explore Menu</button>
+                </div>
+            </div>
+        </div>
+    </div><br>
+
+    <!--Our Services-->
+
+    <div class="container">
+        <div class="row">
+            <div class="col text-center mt-5">
+                <h2><strong>Our Services</strong></h2>
+
+                <div class="contentOS container">
+                    <div class="row">
+                        <div class="col">
+                            <img src="/image/picChildLP1.png" alt="picChild1 Landing Page" style="max-width: 110px; margin-bottom: 1rem;"> <br>
+                            <a class="desOS" style="color:black;">Access a variety of healthy recipes. Easily create nutritious and delicious meals at home</a>
+                        </div>
+                        <div class="col">
+                            <img src="/image/picChildLP2.png" alt="picChild2 Landing Page" style="max-width: 110px; height: auto; margin-bottom: 1rem;"> <br>
+                            <a class="desOS" style="color:black;">Monitor your caloric intake quickly and accurately. Achieve your health goals more effectively</a>
+                        </div>
+                        <div class="col">
+                            <img src="/image/picChildLP3.png" alt="picChild3 Landing Page" style="max-width: 90px; height: auto; margin-bottom: 2rem;"> <br>
+                            <a class="desOS" style="color:black; margin-top:-1rem;">Create a healthy menu according to your preferences. Choose your favorite ingredients and adjust their nutritional proportions with ease.</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><br><br><br>
+
+    <!--Popular Food-->
+
+    <div class="container">
+        <div class="row">
+            <div class="col text-center mt-5">
+                <h2><strong>Popular Food</strong></h2><br>
+                <p>We provide various food recipes with high taste with ratings to see popular food recipes. To see food recipes, go to the "Recipes" menu.</p>
+
+                <div class="contentOS container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="card">
+                                <a>Klean Bowl</a>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <a>Capcay Vegetable</a>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <a>Veg Platter</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,4 @@ Route::get('/Home', function () {
 });
 
 
+
