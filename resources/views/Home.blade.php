4f9da08753d74e0ab348d70bcb5e84dd44293243
aliciaparinussa <aliciaparinussa07@gmail.com>
2024-05-05 21:46:12 +0700

    progress12

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -50,32 +50,36 @@
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <img src="/image/picLogOut.png" alt="Log Out Icon"> Log Out
                                 </a>
-
-                                <!-- Modal -->
-                                <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="logoutModalLabel">Konfirmasi Log Out</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                Apakah Anda yakin ingin keluar?
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                                                <a href="logout.php" class="btn btn-primary">Ya, Log Out</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- End Section Modal -->
                             </div>
                         </div>
                     </li>
 
+                    <!-- Modal Logout Start -->
+                    <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                            <div class="modal-content shadow-lg">
+                                <div class="modal-header bg-gray-200">
+                                    <img src="/image/picLogout2.png" alt="Log Out 2 Icon">
+                                    <h5 class="modal-title text-xm font-weight-bold" id="logoutModalLabel">LOGOUT!</h5>
+                                </div>
+                                <div class="modal-body">
+                                    Are you sure you want to logout? </br>
+                                    Once you logout you need to login again. Are you Ok?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-cancle shadow-lg waves-effect" data-bs-dismiss="modal">
+                                        <i class="fas fa-window-close"></i> Cancel
+                                    </button>
+                                    <button type="button" class="btn btn-logout shadow-lg waves-effect" onclick="logoutAndRedirect()">
+                                        <i class="fas fa-sign-out-alt"></i> Yes, Logout!
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Modal Logout End -->
+
+                    <!-- Section Script profil -->
                     <script>
                         document.addEventListener("DOMContentLoaded", function() {
                             // Mendapatkan nama pengguna dari data yang telah disimpan saat registrasi
@@ -88,7 +92,7 @@
                             }
                         });
                     </script>
-                    <!-- Baru Ditambahin sampai sini-->
+                    <!-- End Section Script profil -->
                 </ul>
             </div>
     </nav>
