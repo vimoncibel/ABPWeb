c1b945fa71f41445f2ad421781a95bce472073bb
aliciaparinussa <aliciaparinussa07@gmail.com>
2024-05-17 10:20:15 +0700

    progress18

--- resources/views/DetailRecipe.blade.php
+++ resources/views/DetailRecipe.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equid="X-UA_Compatible" content="ie=edge">
     <title>Login</title>
-        <!-- Bootstrap CSS -->
+        <!-- Bootstrap CSS --> 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Tautan ke jQuery (diperlukan oleh Bootstrap) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
