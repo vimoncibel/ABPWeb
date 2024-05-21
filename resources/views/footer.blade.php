dcb665b8f1c8c992a193d9c824a5d5e0e6344912
Nur Fitri Lukitania <44227974+lukitania@users.noreply.github.com>
2024-05-22 02:41:53 +0700

    Add files via upload

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,63 @@
+
+<!-- Section Footer-->
+<footer>
+        <div class="container-fluid mt-5" style="background-color: #6C7E46; padding: 20px;">
+                <div class="row mb-4">
+                    <div class="col-md-4 col-sm-4 col-xs-4">
+                        <div class="footer-text">
+                            <div class="brandFooter">
+                                <img src="/image/logoFooter.png" alt="Brand" style="width: 40%;">
+                            </div>
+                                <div class="captionfooter mt-3">
+                                    <p class="card-text" style="margin: 0; line-height: 1.2; color: white;">Unlock a World of Wellness: </p>
+                                    <p class="card-text" style="margin: 0; line-height: 1.2; color: white;">Explore Our Bounty of Nutritious Recipes,</p>
+                                    <p class="card-text" style="margin: 0; line-height: 1.2; color: white;">Crafting Health and Flavor in Every Bite</p>
+                                </div>
+                            <div class="social mt-2 mb-3"> 
+                                <i class="fa fa-instagram fa-lg"></i> 
+                                <i class="fa fa-twitter fa-lg"></i> 
+                                <i class="fa fa-linkedin-square fa-lg"></i> 
+                                <i class="fa fa-facebook"></i> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-4"></div>
+                    
+                    <div class="Feature col-md-2 col-sm-2 col-xs-2 mt-4">
+                        <h5 class="heading">Feature</h5>
+                        <ul class="card-text">
+                            <li>Recipes</li>
+                            <li>Create Your Recipe</li>
+                        </ul>
+                    </div>
+                    <div class="Company col-md-2 col-sm-2 col-xs-2 mt-4">
+                        <h5 class="heading">Company</h5>
+                        <ul class="card-text">
+                            <li>About Us</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="divider mb-4"> </div>
+                
+                <div class="row" style="font-size:10px; margin-left: 30px;">
+                    <div class="col-md-4 col-sm-4 col-xs-4"  style="color: white;">
+                        <div class="pull-left">
+                            <p><i class="fa fa-copyright"></i> 2024 thezpdesign</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4 col-xs-4"></div>
+
+                    <div class="col-md-4 col-sm-4 col-xs-4">
+                        <div class="pull-right mr-4 d-flex policy" style="color: white;">
+                            <div>Terms of Use</div>
+                            <div>Privacy Policy</div>
+                            <div>Cookie Policy</div>
+                        </div>
+                    </div>
+                </div>
+        </div>            
+    </footer>
+    <!-- End Section Footer-->
